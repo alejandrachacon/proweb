a61165ef2aac287c50f5cfce64a2e4081432e9b0
alejandrachacon <alejandra.kcm@gmail.com>
2017-05-29 16:57:48 -0500

    v1 solicitar libros

---
+++ libros/solicitar.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+if (!isset($_SESSION['rol'])) // Solo se puede solicitar si tiene sesion iniciada
+{
+  header('Location: /');
+  exit();
+}
+
+$msg = "";
+
+// Verificar si se hizo un POST para solicitar un equipo
+if (isset($_POST['isbn'], $_POST['titulo'], $_POST['fechaPrestamo']))
+{
+  include_once dirname(__FILE__) . "/libros_crud.php";
+  // Llamar la funcion que registra la solicitud, esta funcion esta en equipos_crud.php
+  if (solicitar_equipo($_SESSION['usuario'], $_POST['isbn'], $_POST['titulo'], $_POST['fechaPrestamo']))
+  {
+    $msg = "<span style='color: green;'>Solicitud enviada con exito</span>";
+  }
+  else
+  {
+    $msg = "<span style='color: red;'>Hubo un error al realizar la solicitud</span>";
+  }
+
+  // TODO: enviar correo de notificacion al admin
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="../css/style.css">
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://unpkg.com/flatpickr/dist/flatpickr.min.css">
+<script src="https://unpkg.com/flatpickr"></script>
+</head>
+<body>
+
+<div class="topnav">
+  <?php
+    if (!isset($_SESSION['rol']))
+    {
+      echo '<a href="../index.php">Inicio</a>';
+    }
+    if (isset($_SESSION['rol']) && $_SESSION['rol'] == 'admin')
+    {
+        echo '<a href="../mensajes.php">Mensajes</a>';
+    }
+  ?>
+  <a href="../eventos.php">Eventos</a>
+  <a href="../salas.php">Salas</a>
+  <a class="active" href="../equipos.php">Equipos</a>
+  <a href="../libros.php">Libros</a>
+  <?php
+		if (isset($_SESSION['rol']))
+		{
+			echo '<a href="../logout.php">Log out</a>';
+		}
+	?>
+</div>
+
+<div class="w3-container" >
+  <h3><?php echo $msg; ?></h3>
+  <h2>Formato de solicitud</h2>
+  <form class="w3-container" action="solicitar.php?isbn=<?php if (isset($_GET['isbn'])) echo $_GET['isbn'] ?>&titulo=<?php if (isset($_GET['titulo'])) echo $_GET['titulo'] ?>" method="POST" style="width: 50%">
+
+    <label>Isbn</label>
+    <input name="isbn" class="w3-input" type="text" value="<?php if (isset($_GET['isbn'])) echo $_GET['isbn'] ?>" readonly>
+
+    <label>Titulo</label>
+    <input name="titulo" class="w3-input" type="text" value="<?php if (isset($_GET['titulo'])) echo $_GET['titulo'] ?>" readonly>
+
+    <label>Fecha de prestamo</label>
+    <br>
+    <input id="fechaPrestamo" name="fechaPrestamo" type="datetime-local" class="form-control" readonly>
+    <br>
+    <br>
+    <input type="submit" value="Solicitar">
+  </form>
+</div>
+
+<script>
+  $(function () {
+    $("#fechaPrestamo").flatpickr({enableTime: true, dateFormat:"Y-m-d H:i", defaultDate: new Date()});
+  });
+</script>
+</body>
+</html>
